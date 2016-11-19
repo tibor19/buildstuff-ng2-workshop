261b33160f4febdf678546ae4956572db0471076
Tiberiu Covaci <tibi@covaci.se>
2016-11-19 15:21:47 +0000

    getting data from params

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './recipe.component.html',
   styleUrls: ['./recipe.component.css']
 })
-export class RecipeComponent implements OnInit {
+export class RecipeComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  constructor(private activeRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    console.log(this.activeRoute.snapshot.params['id']);
+  }
+
+  ngOnChanges() {
+    console.log(this.activeRoute.snapshot.params['id']);
   }
 
 }
